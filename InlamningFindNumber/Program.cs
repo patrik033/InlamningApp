4ccf90774cc05646357fc47f33939f7a0d833a7f
patrik033 <patrik.odh@gmail.com>
2021-09-16 23:21:51 +0200

    Made two methods for extracting the string with the correct numbers, and another for converting that string to a number. Also broke out 3 methods for which print messege goes where.

--- InlamningFindNumber/Program.cs
+++ InlamningFindNumber/Program.cs
@@ -9,8 +9,8 @@ namespace InlamningFindNumber
         {
             string toBeSearched = "29535123p48723487597645723645";
             BigInteger toHoldNumber = 0;
-            int lengthOfString = 0;
-           
+            int lengthOfString;
+            string toNewString = ""; 
             for (int i = 0; i < toBeSearched.Length; i++)
             {
                 char firstOcc = toBeSearched[i];
@@ -18,6 +18,7 @@ namespace InlamningFindNumber
                 lengthOfString = indexOf - i;
                 lengthOfString++;
                 bool isLetters = false;
+                toNewString = "";
 
                 for (int j = i; j <= indexOf; j++)
                 {
@@ -31,39 +32,73 @@ namespace InlamningFindNumber
                 {
                     if (i == 0)
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write(toBeSearched.Substring(i, lengthOfString + i));
-                        Console.ResetColor();
-                        Console.Write(toBeSearched.Substring(indexOf + 1));
-                        Console.WriteLine();
+                        FirsOutput(toBeSearched, ref toHoldNumber, lengthOfString, ref toNewString, i, indexOf);
                     }
 
                     else if (i > 0 && indexOf > 0 && indexOf < toBeSearched.Length - 1)
                     {
-
-
-                        Console.Write(toBeSearched.Substring(0, i));
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write(toBeSearched.Substring(i, lengthOfString));
-                        Console.ResetColor();
-                        Console.Write(toBeSearched.Substring(indexOf + 1));
-                        Console.WriteLine();
+                        MiddleOutput(toBeSearched, ref toHoldNumber, lengthOfString, ref toNewString, i, indexOf);
                     }
 
                     else if (indexOf == toBeSearched.Length - 1)
                     {
-                        Console.Write(toBeSearched.Substring(0, i));
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.Write(toBeSearched.Substring(i));
-                        Console.ResetColor();
-                        Console.WriteLine();
+                        LastOutput(toBeSearched, ref toHoldNumber, ref toNewString, i, indexOf);
                     }
                 }
 
-
-                
             }
+            Console.WriteLine($"\n\n The total sum is: {toHoldNumber}");
+          
             Console.ReadLine();
+        }
+
+        private static void FirsOutput(string toBeSearched, ref BigInteger toHoldNumber, int lengthOfString, ref string toNewString, int i, int indexOf)
+        {
+            toNewString += AddString(toBeSearched, i, indexOf);
+            toHoldNumber += ConvertToNumber(toNewString);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(toBeSearched.Substring(i, lengthOfString + i));
+            Console.ResetColor();
+            Console.Write(toBeSearched.Substring(indexOf + 1));
+            Console.WriteLine();
+        }
+
+        private static void MiddleOutput(string toBeSearched, ref BigInteger toHoldNumber, int lengthOfString, ref string toNewString, int i, int indexOf)
+        {
+            toNewString += AddString(toBeSearched, i, indexOf);
+            toHoldNumber += ConvertToNumber(toNewString);
+            Console.Write(toBeSearched.Substring(0, i));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(toBeSearched.Substring(i, lengthOfString));
+            Console.ResetColor();
+            Console.Write(toBeSearched.Substring(indexOf + 1));
+            Console.WriteLine();
+        }
+
+        private static void LastOutput(string toBeSearched, ref BigInteger toHoldNumber, ref string toNewString, int i, int indexOf)
+        {
+            toNewString += AddString(toBeSearched, i, indexOf);
+            toHoldNumber += ConvertToNumber(toNewString);
+            Console.Write(toBeSearched.Substring(0, i));
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(toBeSearched.Substring(i));
+            Console.ResetColor();
+            Console.WriteLine();
+        }
+
+        private static BigInteger ConvertToNumber(string inputString)
+        {
+            BigInteger tempBig = BigInteger.Parse(inputString);
+            return tempBig;
+        }
+        private static string AddString(string toBeSearched, int i, int indexOf)
+        {
+            string fromCharToString = "";
+            for (int k = i; k <= indexOf; k++)
+            {
+                fromCharToString += toBeSearched[k].ToString();
+            }
+                return fromCharToString;
         }
     }
 }
